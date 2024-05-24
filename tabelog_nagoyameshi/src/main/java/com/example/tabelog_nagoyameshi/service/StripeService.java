4ac8e9cb717c47e1a0a44bbe77d82af727c2ce1d
Yoshidak-ta <bsk.gooserock@gmail.com>
2024-05-24 17:06:21 +0900

    error

--- tabelog_nagoyameshi/src/main/java/com/example/tabelog_nagoyameshi/service/StripeService.java
+++ tabelog_nagoyameshi/src/main/java/com/example/tabelog_nagoyameshi/service/StripeService.java
@@ -91,7 +91,10 @@ public class StripeService {
 	// セッションからメールアドレスと顧客IDを取得し、CardServiceクラスを介してデータベースに登録する
 	public void processSessionCompleted(Event event) {
 		Optional<StripeObject> optionalStripeObject = event.getDataObjectDeserializer().getObject();
-		optionalStripeObject.ifPresent(stripeObject -> {
+		optionalStripeObject.ifPresentOrElse(stripeObject -> {
+			System.out.println("成功");
+			System.out.println("Stripe API Version: " + event.getApiVersion());
+			System.out.println("stripe-java Version: " + Stripe.VERSION);
 			Session session = (Session)stripeObject;
 			String customerId = session.getCustomer();
 			String subscriptionId = session.getSubscription();
@@ -105,8 +108,31 @@ public class StripeService {
 			}catch (StripeException e){
 				e.printStackTrace();
 			}
+		},
+		() -> {
+			System.out.println("失敗");
+			System.out.println("Stripe API Version: " + event.getApiVersion());
+			System.out.println("stripe-java Version: " + Stripe.VERSION);
 		});
 	}
+//	public void processSessionCompleted(Event event) {
+//		Optional<StripeObject> optionalStripeObject = event.getDataObjectDeserializer().getObject();
+//		optionalStripeObject.ifPresent(stripeObject -> {
+//			Session session = (Session)stripeObject;
+//			String customerId = session.getCustomer();
+//			String subscriptionId = session.getSubscription();
+//			
+//			try {
+//				Customer customer = Customer.retrieve(customerId);
+//				String email = customer.getEmail();
+//				
+//				cardService.create(email, customerId, subscriptionId);
+//				userService.roleUpdate(email);
+//			}catch (StripeException e){
+//				e.printStackTrace();
+//			}
+//		});
+//	}
 	
 	//カスタマーポータルセッションの作成
 	public String portalStripeSession(User user, HttpServletRequest httpServletRequest) {
